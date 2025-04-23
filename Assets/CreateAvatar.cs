47d21a165a0f718e6545b15710e89632c5521c5c
Kris Herbert <klh55@pct.edu>
2025-04-23 15:04:50 -0400

    data

--- Assets/CreateAvatar.cs
+++ Assets/CreateAvatar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class CreateAvatar : MonoBehaviour
 {
@@ -21,13 +22,29 @@ public class CreateAvatar : MonoBehaviour
     // Update is called once per frame
     void Submit()
     {
+        StartCoroutine(Upload());
+        //connect to server and send data over
+    }
+
+    IEnumerator Upload()
+    {
         string name = nameInput.text;
         string avatarClass = avatarClassDropdown.options[avatarClassDropdown.value].text;
         float experience = experienceSlider.value;
         string item = itemDropdown.options[itemDropdown.value].text;
 
-        Debug.Log(name + " " + avatarClass + " " + experience + "  " + item);
+        WWWForm form = new WWWForm();
+        form.AddField("name", name);
+        form.AddField("avatarClass", avatarClass);
+        form.AddField("experience", (int)experience);
+        form.AddField("item", item);
 
-        //connect to server and send data over
+        using UnityWebRequest www = UnityWebRequest.Post("http://192.168.56.101/insert.php", form);
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+            Debug.LogError(www.error);
+        else
+            Debug.Log("Form upload complete!");
     }
 }
